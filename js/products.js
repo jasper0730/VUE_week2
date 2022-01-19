b0b217019146b154ef6418008e45c1c688dd1c75
Jasper Li <wilsonlee0730@gmail.com>
2022-01-19 16:00:49 +0800

    first commit

---
+++ js/products.js
@@ -0,0 +1,41 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            apiUrl:'https://vue3-course-api.hexschool.io/v2',
+            apiPath: 'jasper07301',
+            products:[],
+            temp:{},
+        }
+    },
+    methods: {
+        checkAdmin() {
+            const url = `${this.apiUrl}/api/user/check`;
+            axios.post(url)
+            .then((res)=>{
+                this.getData();
+            })
+            .catch((err)=>{
+                console.log(err.data.message)
+            })
+        },
+        getData() {
+            const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
+            axios.get(url)
+            .then((res)=>{
+                this.products = res.data.products;
+            })
+            .catch((err)=>{
+                console.log(err.data.message)
+            })
+        },
+        detailProduct(item) {
+            this.temp = item;
+        }
+    },
+    mounted() {
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+        axios.defaults.headers.common.Authorization = token;
+        this.checkAdmin();
+    },
+})
+app.mount('#app');
